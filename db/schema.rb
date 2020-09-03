279ca249cf1b5fa98963ad9fcd19f7292d5e9213
chunyurina <yurina8ru@gmail.com>
2020-09-03 20:18:30 +0900

    一意性制約マイグレーション

--- db/schema.rb
+++ db/schema.rb
@@ -3,18 +3,20 @@
 # incrementally modify your database, and then regenerate this schema definition.
 #
 # This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, rails db:schema:load tends to
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_01_105541) do
+ActiveRecord::Schema.define(version: 2020_09_03_105431) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
