b7d208698ee5253d41632fa0d18444834e3da220
naoya0240 <yoshioka0240@gmail.com>
2020-09-03 20:46:56 +0900

    一位性制約

--- db/schema.rb
+++ db/schema.rb
@@ -12,11 +12,11 @@
 
 ActiveRecord::Schema.define(version: 2020_09_03_105431) do
 
-  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "email"
-    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
