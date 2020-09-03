4564b7a455c17d8dc33e62f78dcb31ac6fe65b51
rikuto1020 <67956343+rikuto1020@users.noreply.github.com>
2020-09-03 20:26:23 +0900

    '一意性制約'

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_01_105541) do
+ActiveRecord::Schema.define(version: 2020_09_03_105431) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
