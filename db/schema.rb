42a5f65def779cfe07305cf066c1c4c7e75904c6
chunyurina <yurina8ru@gmail.com>
2020-09-04 19:37:23 +0900

    複数のカラムに対するインデックスを設定

--- db/schema.rb
+++ db/schema.rb
@@ -3,14 +3,14 @@
 # incrementally modify your database, and then regenerate this schema definition.
 #
 # This file is the source Rails uses to define your schema when running `rails
-# db:schema:load`. When creating a new database, rails db:schema:load tends to
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
 # be faster and is potentially less error prone than running all of your
 # migrations from scratch. Old migrations may fail to apply correctly if those
 # migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_03_115322) do
+ActiveRecord::Schema.define(version: 2020_09_04_100327) do
 
   create_table "scores", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2020_09_03_115322) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "email"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
